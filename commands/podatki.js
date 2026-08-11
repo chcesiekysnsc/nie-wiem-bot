94e699e9721ccda0ed73a86f7e9393b5a17ffe17
rafal12344321 <rpietra442@gmail.com>
2026-08-12 01:10:14 +0200

    podatki superowe

--- commands/podatki.js
+++ commands/podatki.js
@@ -7,7 +7,13 @@ const TAX_BRACKETS = [
   { min: 2_000_000, max: 10_000_000, rate: 0.08, label: '2mln-10mln' },
   { min: 10_000_000, max: 50_000_000, rate: 0.12, label: '10mln-50mln' },
   { min: 50_000_000, max: 100_000_000, rate: 0.18, label: '50mln-100mln' },
-  { min: 100_000_000, max: Infinity, rate: 0.25, label: '100mln+' }
+  { min: 100_000_000, max: 200_000_000, rate: 0.25, label: '100mln-200mln' },
+  { min: 200_000_000, max: 300_000_000, rate: 0.28, label: '200mln-300mln' },
+  { min: 300_000_000, max: 400_000_000, rate: 0.32, label: '300mln-400mln' },
+  { min: 400_000_000, max: 600_000_000, rate: 0.36, label: '400mln-600mln' },
+  { min: 600_000_000, max: 800_000_000, rate: 0.40, label: '600mln-800mln' },
+  { min: 800_000_000, max: 999_000_000, rate: 0.45, label: '800mln-999mln' },
+  { min: 999_000_000, max: Infinity, rate: 0.50, label: '999mln+' }
 ];
 
 function calculateProgressiveTax(wealth, hasKsiegowa = false) {
