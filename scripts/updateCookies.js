ca7e3f367f876b91cb2aa287b706edad805b3491
rafal12344321 <rpietra442@gmail.com>
2026-08-29 16:06:30 +0200

    update cookies timestamp in appstate.json

---
+++ scripts/updateCookies.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const path = require('path');
+
+const NEW_COOKIES = [
+    { "key": "dbln", "value": "%7B%2261562475523609%22%3A%22AX6WwYPo%22%7D", "domain": "facebook.com", "path": "/login/device-based/", "hostOnly": false, "creation": "2026-08-29T14:02:20.221Z", "lastAccessed": "2026-08-29T14:02:20.221Z" },
+    { "key": "sb", "value": "oZ-mZmUkSi-ORxWZSYx0LUyc", "domain": "facebook.com", "path": "/", "hostOnly": false, "creation": "2026-08-29T14:02:20.221Z", "lastAccessed": "2026-08-29T14:02:20.221Z" },
+    { "key": "oo", "value": "v1", "domain": "facebook.com", "path": "/", "hostOnly": false, "creation": "2026-08-29T14:02:20.221Z", "lastAccessed": "2026-08-29T14:02:20.221Z" },
+    { "key": "datr", "value": "vWo9aRvRclEH-d95BN9Q5ptx", "domain": "facebook.com", "path": "/", "hostOnly": false, "creation": "2026-08-29T14:02:20.221Z", "lastAccessed": "2026-08-29T14:02:20.221Z" },
+    { "key": "ps_l", "value": "1", "domain": "facebook.com", "path": "/", "hostOnly": false, "creation": "2026-08-29T14:02:20.221Z", "lastAccessed": "2026-08-29T14:02:20.221Z" },
+    { "key": "ps_n", "value": "1", "domain": "facebook.com", "path": "/", "hostOnly": false, "creation": "2026-08-29T14:02:20.221Z", "lastAccessed": "2026-08-29T14:02:20.221Z" },
+    { "key": "wd", "value": "1517x712", "domain": "facebook.com", "path": "/", "hostOnly": false, "creation": "2026-08-29T14:02:20.221Z", "lastAccessed": "2026-08-29T14:02:20.221Z" },
+    { "key": "dpr", "value": "0.8999999761581421", "domain": "facebook.com", "path": "/", "hostOnly": false, "creation": "2026-08-29T14:02:20.221Z", "lastAccessed": "2026-08-29T14:02:20.221Z" },
+    { "key": "c_user", "value": "61560227271099", "domain": "facebook.com", "path": "/", "hostOnly": false, "creation": "2026-08-29T14:02:20.222Z", "lastAccessed": "2026-08-29T14:02:20.222Z" },
+    { "key": "fr", "value": "0wHcViNGlb767Riz6.AWccu18lxofeIZDQ-j3bfx07FFXhMhiQ0Gu1NOWi1Ltpef8b3w0.BqkuZk..AAA.0.0.BqkuZk.AWfggzVZHYuBnT6BVX5yeQa7ThQ", "domain": "facebook.com", "path": "/", "hostOnly": false, "creation": "2026-08-29T14:02:20.222Z", "lastAccessed": "2026-08-29T14:02:20.222Z" },
+    { "key": "xs", "value": "44%3A8y014dYqJON3EA%3A2%3A1788012130%3A-1%3A-1%3A%3AAcz_G44qWT87nTiEZzRR_8CLFfX7O01tesK3sSngCA", "domain": "facebook.com", "path": "/", "hostOnly": false, "creation": "2026-08-29T14:02:20.222Z", "lastAccessed": "2026-08-29T14:02:20.222Z" },
+    { "key": "presence", "value": "C%7B%22t3%22%3A%5B%5D%2C%22utc3%22%3A1788012138068%2C%22v%22%3A1%7D", "domain": "facebook.com", "path": "/", "hostOnly": false, "creation": "2026-08-29T14:02:20.222Z", "lastAccessed": "2026-08-29T14:02:20.222Z" }
+];
+
+const DATA_APPSTATE = path.join(__dirname, '..', 'data', 'appstate.json');
+const SEED_APPSTATE = path.join(__dirname, '..', 'data_seed', 'appstate.json');
+
+function mergeCookies(existing, updates) {
+    const map = new Map(existing.map(c => [c.key, { ...c }]));
+    for (const u of updates) {
+        const existingEntry = map.get(u.key);
+        if (existingEntry) {
+            existingEntry.value = u.value;
+            existingEntry.creation = u.creation;
+            existingEntry.lastAccessed = u.lastAccessed;
+        } else {
+            map.set(u.key, { ...u });
+        }
+    }
+    return Array.from(map.values());
+}
+
+function main() {
+    let data = [];
+    try {
+        data = JSON.parse(fs.readFileSync(DATA_APPSTATE, 'utf8'));
+    } catch (_) {}
+    data = mergeCookies(data, NEW_COOKIES);
+    fs.writeFileSync(DATA_APPSTATE, JSON.stringify(data, null, 2), 'utf8');
+    console.log('[COOKIES] Zaktualizowano data/appstate.json');
+
+    fs.copyFileSync(DATA_APPSTATE, SEED_APPSTATE);
+    console.log('[COOKIES] Skopiowano do data_seed/appstate.json');
+}
+
+main();
