004f8cd06ca2120a89214d504e6f01a3e84f6d3a
rafal12344321 <rpietra442@gmail.com>
2026-09-10 15:53:16 +0200

    Update cookies to 2026-09-10T13:51:11Z

--- scripts/updateCookies.js
+++ scripts/updateCookies.js
@@ -8,8 +8,8 @@ const NEW_COOKIES = [
         "domain": "facebook.com",
         "path": "/login/device-based/",
         "hostOnly": false,
-        "creation": "2026-09-08T21:35:23.653Z",
-        "lastAccessed": "2026-09-08T21:35:23.654Z"
+        "creation": "2026-09-10T13:51:11.153Z",
+        "lastAccessed": "2026-09-10T13:51:11.153Z"
     },
     {
         "key": "sb",
@@ -17,8 +17,8 @@ const NEW_COOKIES = [
         "domain": "facebook.com",
         "path": "/",
         "hostOnly": false,
-        "creation": "2026-09-08T21:35:23.654Z",
-        "lastAccessed": "2026-09-08T21:35:23.654Z"
+        "creation": "2026-09-10T13:51:11.153Z",
+        "lastAccessed": "2026-09-10T13:51:11.153Z"
     },
     {
         "key": "datr",
@@ -26,8 +26,8 @@ const NEW_COOKIES = [
         "domain": "facebook.com",
         "path": "/",
         "hostOnly": false,
-        "creation": "2026-09-08T21:35:23.654Z",
-        "lastAccessed": "2026-09-08T21:35:23.654Z"
+        "creation": "2026-09-10T13:51:11.153Z",
+        "lastAccessed": "2026-09-10T13:51:11.153Z"
     },
     {
         "key": "ps_l",
@@ -35,8 +35,8 @@ const NEW_COOKIES = [
         "domain": "facebook.com",
         "path": "/",
         "hostOnly": false,
-        "creation": "2026-09-08T21:35:23.654Z",
-        "lastAccessed": "2026-09-08T21:35:23.654Z"
+        "creation": "2026-09-10T13:51:11.153Z",
+        "lastAccessed": "2026-09-10T13:51:11.154Z"
     },
     {
         "key": "ps_n",
@@ -44,8 +44,8 @@ const NEW_COOKIES = [
         "domain": "facebook.com",
         "path": "/",
         "hostOnly": false,
-        "creation": "2026-09-08T21:35:23.654Z",
-        "lastAccessed": "2026-09-08T21:35:23.654Z"
+        "creation": "2026-09-10T13:51:11.154Z",
+        "lastAccessed": "2026-09-10T13:51:11.154Z"
     },
     {
         "key": "dpr",
@@ -53,17 +53,17 @@ const NEW_COOKIES = [
         "domain": "facebook.com",
         "path": "/",
         "hostOnly": false,
-        "creation": "2026-09-08T21:35:23.654Z",
-        "lastAccessed": "2026-09-08T21:35:23.654Z"
+        "creation": "2026-09-10T13:51:11.154Z",
+        "lastAccessed": "2026-09-10T13:51:11.154Z"
     },
     {
         "key": "wd",
-        "value": "1510x721",
+        "value": "1517x712",
         "domain": "facebook.com",
         "path": "/",
         "hostOnly": false,
-        "creation": "2026-09-08T21:35:23.654Z",
-        "lastAccessed": "2026-09-08T21:35:23.654Z"
+        "creation": "2026-09-10T13:51:11.154Z",
+        "lastAccessed": "2026-09-10T13:51:11.154Z"
     },
     {
         "key": "c_user",
@@ -71,49 +71,48 @@ const NEW_COOKIES = [
         "domain": "facebook.com",
         "path": "/",
         "hostOnly": false,
-        "creation": "2026-09-08T21:35:23.654Z",
-        "lastAccessed": "2026-09-08T21:35:23.654Z"
+        "creation": "2026-09-10T13:51:11.154Z",
+        "lastAccessed": "2026-09-10T13:51:11.154Z"
     },
     {
         "key": "fr",
-        "value": "0zrCiC44RLYHMEQGb.AWcpDLYAwMMdRFNsToyekbpinJ3k3ihCcwSH1H5uR3pSZiKiQXU.BqoH-W..AAA.0.0.BqoH-W.AWdeD4a_mZhXF6L1apcPrB4VKDA",
+        "value": "0lv9VyPqYMCo82ht1.AWdWD6XvGrV_rk-vzMArF63glJKuddF_YedPbkt9O9oek-v6lao.BqorXE..AAA.0.0.BqorXE.AWeiXLWI5y062eIDCjI12GnJruk",
         "domain": "facebook.com",
         "path": "/",
         "hostOnly": false,
-        "creation": "2026-09-08T21:35:23.654Z",
-        "lastAccessed": "2026-09-08T21:35:23.654Z"
+        "creation": "2026-09-10T13:51:11.154Z",
+        "lastAccessed": "2026-09-10T13:51:11.154Z"
     },
     {
         "key": "xs",
-        "value": "33%3AuYf9jBqqF4fuvg%3A2%3A1788903316%3A-1%3A-1%3A%3AAcwLgHUZZfdbhfTWXXjPHKBqdbS3xE60ivxr3i4WXA",
+        "value": "10%3Aiv7ZDb37gZxAmg%3A2%3A1789048256%3A-1%3A-1%3A%3AAczaBrO1_FKHFdiEWVYgRHD5AbpU8eYRdIxlFRAiGQ",
         "domain": "facebook.com",
         "path": "/",
         "hostOnly": false,
-        "creation": "2026-09-08T21:35:23.656Z",
-        "lastAccessed": "2026-09-08T21:35:23.656Z"
+        "creation": "2026-09-10T13:51:11.154Z",
+        "lastAccessed": "2026-09-10T13:51:11.154Z"
     },
     {
         "key": "presence",
-        "value": "C%7B%22t3%22%3A%5B%5D%2C%22utc3%22%3A1788903321665%2C%22v%22%3A1%7D",
+        "value": "C%7B%22t3%22%3A%5B%5D%2C%22utc3%22%3A1789048268287%2C%22v%22%3A1%7D",
         "domain": "facebook.com",
         "path": "/",
         "hostOnly": false,
-        "creation": "2026-09-08T21:35:23.656Z",
-        "lastAccessed": "2026-09-08T21:35:23.656Z"
+        "creation": "2026-09-10T13:51:11.154Z",
+        "lastAccessed": "2026-09-10T13:51:11.154Z"
     }
 ];
 
-const ROOT_APPSTATE = path.join(__dirname, '..', 'appstate.json');
 const SEED_APPSTATE = path.join(__dirname, '..', 'data_seed', 'appstate.json');
-const DATA_APPSTATE = path.join(__dirname, '..', 'data', 'appstate.json');
-const BACKUP_DIR = 'C:\\Users\\dupek\\.gemini\\antigravity\\db_backups';
 
 function mergeCookies(existing, updates) {
     const map = new Map(existing.map(c => [c.key, { ...c }]));
     for (const u of updates) {
         const existingEntry = map.get(u.key);
         if (existingEntry) {
-            Object.assign(existingEntry, u);
+            existingEntry.value = u.value;
+            existingEntry.creation = u.creation;
+            existingEntry.lastAccessed = u.lastAccessed;
         } else {
             map.set(u.key, { ...u });
         }
@@ -127,36 +126,8 @@ function main() {
         data = JSON.parse(fs.readFileSync(SEED_APPSTATE, 'utf8'));
     } catch (_) {}
     data = mergeCookies(data, NEW_COOKIES);
-
-    const jsonStr = JSON.stringify(data, null, 2);
-
-    // 1. Root appstate.json
-    fs.writeFileSync(ROOT_APPSTATE, jsonStr, 'utf8');
-    console.log('[COOKIES] Zaktualizowano appstate.json');
-
-    // 2. data_seed/appstate.json
-    fs.writeFileSync(SEED_APPSTATE, jsonStr, 'utf8');
+    fs.writeFileSync(SEED_APPSTATE, JSON.stringify(data, null, 2), 'utf8');
     console.log('[COOKIES] Zaktualizowano data_seed/appstate.json');
-
-    // 3. data/appstate.json
-    try {
-        const dataDir = path.dirname(DATA_APPSTATE);
-        if (!fs.existsSync(dataDir)) fs.mkdirSync(dataDir, { recursive: true });
-        fs.writeFileSync(DATA_APPSTATE, jsonStr, 'utf8');
-        console.log('[COOKIES] Zaktualizowano data/appstate.json');
-    } catch (e) {
-        console.error('Błąd zapisu data/appstate.json:', e.message);
-    }
-
-    // 4. db_backups/appstate.json
-    if (fs.existsSync(BACKUP_DIR)) {
-        try {
-            fs.writeFileSync(path.join(BACKUP_DIR, 'appstate.json'), jsonStr, 'utf8');
-            console.log('[COOKIES] Zaktualizowano db_backups/appstate.json');
-        } catch (e) {
-            console.error('Błąd zapisu db_backups/appstate.json:', e.message);
-        }
-    }
 }
 
 main();
